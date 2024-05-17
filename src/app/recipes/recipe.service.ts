663ee02acdac1003197df0af3441a2fa3892c013
Sameeksha Murdia <sameeksha.murdia.phy20@itbhu.ac.in>
2024-05-18 01:24:15 +0530

    Routing in are project

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,35 @@
-import { EventEmitter, Injectable } from "@angular/core";
-import { Recipe } from "./recipe.model";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
-export class RecipeService{
-    recipeSelected = new EventEmitter<Recipe>();
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
 
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Oreo-Two-Cookies.png/1200px-Oreo-Two-Cookies.png', [new Ingredient('Milk', 1), new Ingredient('Oreo', 4)]),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/McDonald%27s_Quarter_Pounder_with_Cheese%2C_United_States.jpg/1200px-McDonald%27s_Quarter_Pounder_with_Cheese%2C_United_States.jpg', [new Ingredient('Bun', 1), new Ingredient('patty', 2)])
-      ];
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Oreo Shake',
+      'A super-tasty drink',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Oreo-Two-Cookies.png/1200px-Oreo-Two-Cookies.png',
+      [new Ingredient('Milk', 1), new Ingredient('Oreo', 4)]
+    ),
+    new Recipe(
+      'Double Patty burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/McDonald%27s_Quarter_Pounder_with_Cheese%2C_United_States.jpg/1200px-McDonald%27s_Quarter_Pounder_with_Cheese%2C_United_States.jpg',
+      [new Ingredient('Bun', 1), new Ingredient('patty', 2)]
+    ),
+  ];
 
-    constructor(private shoppingListService: ShoppingListService) { }
-    getRecipes(){
-        return this.recipes.slice();
-    }
-    addingIngredientsToShoppingList(ingredients: Ingredient[]){
-        this.shoppingListService.addIngredients(ingredients);
-}
+  constructor(private shoppingListService: ShoppingListService) {}
+  getRecipes() {
+    return this.recipes.slice();
+  }
+  addingIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 }
